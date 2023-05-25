f627d017effab040f4cb7931ac1636a285c9545b
Phuc Le Khac <lkp@aod.vn>
2023-05-25 17:12:49 +0700

    task fix error white page

--- BPHN.MVC/admin/src/layouts/BPHNLayout.vue
+++ BPHN.MVC/admin/src/layouts/BPHNLayout.vue
@@ -1,23 +1,3 @@
-<script setup>
-import AccountAPI from '@/apis/AccountAPI'
-import { loadConfig } from '@/loader'
-import { useStore } from 'vuex'
-import { useI18n } from 'vue-i18n'
-
-const t = useI18n()
-const store = useStore()
-
-let validateResult = await AccountAPI.validateToken(store.getters['account/getToken'])
-if (validateResult?.data?.success) {
-  loadConfig()
-  t.locale.value = window["Language"]
-}
-else {
-  window.location = '/login'
-}
-
-</script>
-
 <template>
   <div class="common-layout">
     <el-container>
@@ -39,8 +19,24 @@ else {
 <script>
 import BphnHeader from '@/layouts/BPHNHeader.vue'
 import BphnMenu from '@/layouts/BPHNMenu.vue'
+import AccountAPI from '@/apis/AccountAPI'
+import { loadConfig } from '@/loader'
+import { useStore } from 'vuex'
+import { useI18n } from 'vue-i18n'
 export default {
   name: "BPHNLayout",
   components: { BphnHeader, BphnMenu },
+  async created() {
+    const store = useStore()
+    const t = useI18n()
+    let validateResult = await AccountAPI.validateToken(store.getters['account/getToken'])
+    if (validateResult?.data?.success) {
+      loadConfig()
+      t.locale.value = window["Language"]
+    }
+    else {
+      window.location = '/login'
+    }
+  }
 };
 </script>
