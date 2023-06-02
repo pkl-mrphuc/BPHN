50a19d0e324e41ca96ad66ba14cc82166ad42ff4
Phuc Le Khac <lkp@aod.vn>
2023-06-02 16:52:16 +0700

    task client football field

--- BPHN.MVC/admin/src/components/dialogs/BPHNFootballFieldDialog.vue
+++ BPHN.MVC/admin/src/components/dialogs/BPHNFootballFieldDialog.vue
@@ -1,37 +1,57 @@
 <script setup>
-import { computed, ref } from "vue"
+import { computed, ref, onMounted, nextTick } from "vue"
 import { Location } from "@element-plus/icons-vue"
 import useToggleModal from '@/register-components/actionDialog'
 
 const { toggleModel } = useToggleModal()
-
 const configData = [
   {
-    name: "Giá",
+    name: "Giá (Nghìn đồng)",
+    type: 'input'
   },
   {
-    name: "Bắt đầu lúc",
+    name: "Giờ vào",
+    type: 'time-picker'
   },
   {
-    name: "Kết thúc lúc",
+    name: "Giờ ra",
+    type: 'time-picker'
   }
 ]
 
 const status = ref('shanghai')
 const name = ref('')
 const address = ref('')
-const quantity = ref('')
-const minutesPerMatch = ref('')
-const timeSlotPerDay = ref('')
-const isShowConfigTimeSlotTable = computed(() => {
-  if(timeSlotPerDay.value && !isNaN(Number(timeSlotPerDay.value))) return true
-  return false 
+
+const quantity = ref(1)
+const minutesPerMatch = ref(90)
+const timeSlotPerDay = ref(1)
+
+const lstTimeSlot = computed(() => {
+  let result = []
+  for (let i = 0; i < timeSlotPerDay.value; i++) {
+    result.push({
+      name: `Khung ${i+1}`
+    })
+  }
+  return result
 })
+
+const maxTimeSlot = computed(() => {
+  return 1440 / minutesPerMatch.value
+})
+
+onMounted(() => {
+  nextTick(() => {
+    document.getElementById('inpTimeSlot').setAttribute('readonly', true)
+  })
+})
+
 </script>
 
 
 <template>
-  <Dialog :title="'Thêm mới sân bóng'" :width="900">
+  <Dialog :title="'Thêm mới sân bóng'" :width="1200">
     <template #body>
       <el-form>
         <el-form-item>
@@ -52,25 +72,36 @@ const isShowConfigTimeSlotTable = computed(() => {
           </el-col>
         </el-form-item>
         <el-form-item>
-          <el-col :span="7">
-            <el-input v-model="quantity" placeholder="Số lượng sân" />
+          <el-col :span="8" class="center">
+            <div>Số lượng sân</div>
+            <el-input-number v-model="quantity" :min="1" :max="100" />
           </el-col>
-          <el-col :span="7">
-            <el-input v-model="minutesPerMatch" placeholder="Số phút / trận" />
+          <el-col :span="8" class="center">
+            <div>Số phút / trận</div>
+            <el-input-number v-model="minutesPerMatch" :min="30" :max="1440" :step="30" />
           </el-col>
-          <el-col :span="7">
-            <el-input v-model="timeSlotPerDay" placeholder="Khung giờ / ngày" />
+          <el-col :span="8" class="center">
+            <div>Khung giờ / ngày</div>
+            <el-input-number id="inpTimeSlot" v-model="timeSlotPerDay" :min="1" :max="maxTimeSlot" />
           </el-col>
         </el-form-item>
-        <el-form-item v-if="isShowConfigTimeSlotTable">
-          <el-table :data="configData" style="width: 100%">
-            <el-table-column label="">
+        <el-form-item>
+          <el-table :data="configData" class="w100">
+            <el-table-column label="" width="200">
               <template #default="scope">
                 <span>{{ scope.row.name }}</span>
               </template>
             </el-table-column>
-            <el-table-column label="Khung 1"></el-table-column>
-            <el-table-column label="Khung 2"> </el-table-column>
+
+            <el-table-column v-for="item in lstTimeSlot" 
+                              :key="item" 
+                              :label="item.name"
+                              :min-width="160">
+              <template #default="scope">
+                <el-input-number v-if="scope.row.type == 'input'" class="w100" :min="0" :step="100" />
+                <el-time-picker v-if="scope.row.type == 'time-picker'" class="w100" />
+              </template>
+            </el-table-column>
           </el-table>
         </el-form-item>
       </el-form>
@@ -84,3 +115,14 @@ const isShowConfigTimeSlotTable = computed(() => {
   </Dialog>
 </template>
 
+<style scoped>
+.center {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.w100 {
+  width: 100%;
+}
+</style>
