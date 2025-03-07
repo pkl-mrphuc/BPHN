1b97e8892402e392fe155d997f0013e4bc1111cb
Phuc Le Khac <lkp@aod.vn>
2025-03-07 11:15:54 +0700

    task refactor config

--- BPHN.DataLayer/ImpRepositories/ConfigRepository.cs
+++ BPHN.DataLayer/ImpRepositories/ConfigRepository.cs
@@ -14,25 +14,12 @@ namespace BPHN.DataLayer.ImpRepositories
 
         public async Task<List<Config>> GetConfigs(Guid accountId, string? key = null)
         {
-            using (var connection = ConnectDB(GetConnectionString()))
-            {
-                connection.Open();
-                Dictionary<string, object> dic = new Dictionary<string, object>();
-                dic.Add("@accountId", accountId);
-                var query = $"select c.Key, c.Value from configs c where c.AccountId = @accountId";
-                if (!string.IsNullOrWhiteSpace(key))
-                {
-                    dic.Add("@key", key);
-                    query = $"select c.Key, c.Value from configs c where c.AccountId = @accountId and c.Key in (@key)";
-                }
-                var configs = (await connection.QueryAsync<Config>(query, dic)).ToList();
-                return configs;
-            }
+            return !string.IsNullOrWhiteSpace(key) ? await GetConfigsByKey(accountId, key) : await GetConfigs(accountId);
         }
 
         public async Task<bool> Save(List<Config> configs)
         {
-            using (var connection = ConnectDB(GetConnectionString())) 
+            using (var connection = ConnectDB(GetConnectionString()))
             {
                 connection.Open();
                 using (var tranction = connection.BeginTransaction())
@@ -54,7 +41,7 @@ namespace BPHN.DataLayer.ImpRepositories
                                             value(@idConfig, @accountId, @key, @value, @createdDate, @createdBy, @modifiedDate, @modifiedBy)
                                         on duplicate key update Value = @value, ModifiedDate = @modifiedDate, ModifiedBy = @modifiedBy";
                         var affect = await connection.ExecuteAsync(query, dic, tranction, commandType: System.Data.CommandType.Text);
-                        if(affect == 0)
+                        if (affect == 0)
                         {
                             tranction.Rollback();
                             return false;
@@ -64,6 +51,33 @@ namespace BPHN.DataLayer.ImpRepositories
                 }
             }
             return true;
+        }
+
+        private async Task<List<Config>> GetConfigs(Guid accountId)
+        {
+            using (var connection = ConnectDB(GetConnectionString()))
+            {
+                connection.Open();
+                var configs = (await connection.QueryAsync<Config>(Query.CONFIG_GET_ALL, new Dictionary<string, object>
+                    {
+                        { "@accountId", accountId }
+                    })).ToList();
+                return configs;
+            }
+        }
+
+        private async Task<List<Config>> GetConfigsByKey(Guid accountId, string key)
+        {
+            using (var connection = ConnectDB(GetConnectionString()))
+            {
+                connection.Open();
+                var configs = (await connection.QueryAsync<Config>(Query.CONFIG_GET_BY_KEY, new Dictionary<string, object>
+                {
+                    { "@accountId", accountId },
+                    { "@key", key }
+                })).ToList();
+                return configs;
+            }
         }
     }
 }
