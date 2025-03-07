dc81a9364490552b7803b449712051e3ba467791
Phuc Le Khac <lkp@aod.vn>
2025-03-07 11:15:54 +0700

    task refactor config

--- BPHN.ModelLayer/Constansts.cs
+++ BPHN.ModelLayer/Constansts.cs
@@ -41,5 +41,8 @@
         public const string BOOKING_DETAIL__GET_BY_BOOKING_ID = "select * from booking_details where BookingId = @bookingId";
         public const string BOOKING_DETAIL__GET_BY_ID = "select * from booking_details where Id = @id";
         public const string BOOKING_DETAIL__UPDATE_MATCH = "update booking_details set TeamA = @teamA, TeamB = @teamB, Note = @note, Deposite = @deposite where Id = @id";
+
+        public const string CONFIG_GET_ALL = "select c.Key, c.Value from configs c where c.AccountId = @accountId";
+        public const string CONFIG_GET_BY_KEY = "select c.Key, c.Value from configs c where c.AccountId = @accountId and c.Key in (@key)";
     }
 }
