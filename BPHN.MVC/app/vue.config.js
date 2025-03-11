e1f48c44e892ab7bffc4477ea1bbcd4cd234da54
Nguyễn Lương Khang <96479575+Khangnekk@users.noreply.github.com>
2025-03-11 22:56:57 +0700

    Update vue.config.js

--- BPHN.MVC/app/vue.config.js
+++ BPHN.MVC/app/vue.config.js
@@ -1,5 +1,4 @@
-const { defineConfig } = require('@vue/cli-service');
-require("dotenv").config();
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
