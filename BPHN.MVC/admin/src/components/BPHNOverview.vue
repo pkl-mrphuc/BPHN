a1f947cdc12b7a1b6788471023e505b3ec0b1d40
Phuc Le Khac <lkp@aod.vn>
2025-03-17 18:11:58 +0700

    task overview

--- BPHN.MVC/admin/src/components/BPHNOverview.vue
+++ BPHN.MVC/admin/src/components/BPHNOverview.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { useI18n } from "vue-i18n";
-
+import { CaretTop } from "@element-plus/icons-vue";
+import { BookingStatusEnum } from "@/const";
 const { t } = useI18n();
 </script>
 
@@ -8,107 +9,140 @@ const { t } = useI18n();
     <section>
         <div class="container">
             <div class="row mb-3 d-flex flex-row align-items-center justify-content-between">
-                <h3 class="fs-3 col-12 col-sm-12 col-md-12 col-lg-8">
-                    {{ t("Overview") }}
-                </h3>
-                <div class="col-12 col-sm-12 col-md-12 col-lg-4 d-flex flex-row">
+                <h3 class="fs-3 col-12 col-sm-12 col-md-12 col-lg-8">{{ t("Overview") }}</h3>
+                <div class="col-12 col-sm-12 col-md-12 col-lg-4 d-flex flex-row-reverse">
+                    <el-button type="primary">Tùy chỉnh</el-button>
                 </div>
             </div>
-            <div>
-                <el-row :gutter="16">
-                    <el-col :span="8">
-                        <div class="statistic-card">
-                            <el-statistic :value="98500">
-                                <template #title>
-                                    <div style="display: inline-flex; align-items: center">
-                                        Booking chờ duyệt
-                                        <el-tooltip effect="dark"
-                                            content=""
-                                            placement="top">
-                                            <el-icon style="margin-left: 4px" :size="12">
-                                                <Warning />
-                                            </el-icon>
-                                        </el-tooltip>
+            <div class="row">
+                <div class="col-12 col-sm-12 col-md-12 col-lg-8">
+                    <div class="mr-4">
+                        <el-card class="mt-4 mb-4">
+                            <div class="row">
+                                <el-card class="col-12 col-sm-12 col-md-12 col-lg-4">
+                                    <el-statistic :value="98500">
+                                        <template #title>
+                                            <div class="d-flex flex-row align-items-center">
+                                                <h2 class="m-0">Doanh thu tháng 3</h2>
+                                            </div>
+                                        </template>
+                                    </el-statistic>
+                                    <div class="statistic-footer">
+                                        <div class="footer-item">
+                                            <span>than yesterday</span>
+                                            <span class="green">24%
+                                                <el-icon><CaretTop /></el-icon>
+                                            </span>
+                                        </div>
                                     </div>
-                                </template>
-                            </el-statistic>
-                            <div class="statistic-footer">
-                                <div class="footer-item">
-                                    <span></span>
-                                    <span class="green">
-                                        <el-icon>
-                                            <CaretTop />
-                                        </el-icon>
-                                    </span>
+                                </el-card>
+                                <el-card class="col-12 col-sm-12 col-md-12 col-lg-6">
+                                    <div class="d-flex flex-row align-items-center justify-content-between">
+                                        <el-card>
+                                            <el-statistic :value="98500">
+                                                <template #title>
+                                                    <div class="d-flex flex-row align-items-center">
+                                                        <h2 class="m-0">{{ t(BookingStatusEnum.PENDING) }}</h2>
+                                                    </div>
+                                                </template>
+                                            </el-statistic>
+                                        </el-card>
+                                        <el-card>
+                                            <el-statistic :value="98500">
+                                                <template #title>
+                                                    <div class="d-flex flex-row align-items-center">
+                                                        <h2 class="m-0">{{ t(BookingStatusEnum.SUCCESS) }}</h2>
+                                                    </div>
+                                                </template>
+                                            </el-statistic>
+                                        </el-card>
+                                        <el-card>
+                                            <el-statistic :value="98500">
+                                                <template #title>
+                                                    <div class="d-flex flex-row align-items-center">
+                                                        <h2 class="m-0">{{ t(BookingStatusEnum.CANCEL) }}</h2>
+                                                    </div>
+                                                </template>
+                                            </el-statistic>
+                                        </el-card>
+                                    </div>
+                                </el-card>
+                                <el-card class="col-12 col-sm-12 col-md-12 col-lg-2">
+                                    <div class="d-flex flex-row justify-content-end">
+                                        <div class="d-flex flex-column">
+                                            <el-button text class="m-0">Chi tiết</el-button>
+                                            <el-button class="m-0" type="danger">Từ chối</el-button>
+                                            <el-button class="m-0" type="primary">Phê duyệt</el-button>
+                                        </div>
+                                    </div>
+                                </el-card>
+                            </div>
+                        </el-card>
+                        <el-card class="mt-4 mb-4">
+                            <div class="d-flex flex-row align-items-center justify-content-between mb-3">
+                                <div>
+                                    <h2 class="m-0">Doanh thu của bạn</h2>
+                                    <span>01/03/2025 - 31/03/2025</span>
+                                </div>
+                                <el-button text class="m-0">Chi tiết</el-button>
+                            </div>
+                            <div class="row">
+                                <div class="col-12 col-sm-12 col-md-12 col-lg-6">
+                                    <el-card></el-card>
+                                </div>
+                                <div class="col-12 col-sm-12 col-md-12 col-lg-6">
+                                    <el-card></el-card>
                                 </div>
                             </div>
-                        </div>
-                    </el-col>
-                    <el-col :span="8">
-                        <div class="statistic-card">
-                            <el-statistic :value="693700">
-                                <template #title>
-                                    <div style="display: inline-flex; align-items: center">
-                                        Doanh thu hôm nay
-                                        <el-tooltip effect="dark"
-                                            content=""
-                                            placement="top">
-                                            <el-icon style="margin-left: 4px" :size="12">
-                                                <Warning />
-                                            </el-icon>
-                                        </el-tooltip>
+                        </el-card>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-12 col-md-12 col-lg-4">
+                    <div class="d-flex flex-column">
+                        <div class="ml-4">
+                            <el-card class="mt-4 mb-4">
+                                <el-statistic :value="98500">
+                                    <template #title>
+                                        <div class="d-flex flex-row align-items-center">
+                                            <h2 class="m-0">Doanh thu tháng 3</h2>
+                                        </div>
+                                    </template>
+                                </el-statistic>
+                                <div class="statistic-footer">
+                                    <div class="footer-item">
+                                        <span>than yesterday</span>
+                                        <span class="green">24%
+                                            <el-icon><CaretTop /></el-icon>
+                                        </span>
                                     </div>
-                                </template>
-                            </el-statistic>
-                            <div class="statistic-footer">
-                                <div class="footer-item">
-                                    <span></span>
-                                    <span class="red">
-                                        <el-icon>
-                                            <CaretBottom />
-                                        </el-icon>
-                                    </span>
                                 </div>
-                            </div>
-                        </div>
-                    </el-col>
-                    <el-col :span="8">
-                        <div class="statistic-card">
-                            <el-statistic :value="72000" title="New transactions today">
-                                <template #title>
-                                    <div style="display: inline-flex; align-items: center">
-                                        Doanh thu tháng 3
+                            </el-card>
+                            <el-card class="mt-4 mb-4">
+                                <el-statistic :value="98500">
+                                    <template #title>
+                                        <div class="d-flex flex-row align-items-center">
+                                            <h2 class="m-0">Tổng hóa đơn</h2>
+                                        </div>
+                                    </template>
+                                </el-statistic>
+                                <div class="statistic-footer">
+                                    <div class="footer-item">
+                                        <span>than yesterday</span>
+                                        <span class="green">24%
+                                            <el-icon><CaretTop /></el-icon>
+                                        </span>
                                     </div>
-                                </template>
-                            </el-statistic>
-                            <div class="statistic-footer">
-                                <div class="footer-item">
-                                    <span></span>
-                                    <span class="green">
-                                        <el-icon>
-                                            <CaretTop />
-                                        </el-icon>
-                                    </span>
                                 </div>
-                                <div class="footer-item">
-                                    <el-icon :size="14">
-                                        <ArrowRight />
-                                    </el-icon>
-                                </div>
-                            </div>
+                            </el-card>
                         </div>
-                    </el-col>
-                </el-row>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
 </template>
 
 <style scoped>
-:global(h2#card-usage ~ .example .example-showcase) {
-    background-color: var(--el-fill-color) !important;
-}
-
 .el-statistic {
     --el-statistic-content-font-size: 28px;
 }
