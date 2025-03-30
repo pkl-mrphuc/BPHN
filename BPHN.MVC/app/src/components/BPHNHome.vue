d8a9a8dbaa3d0fb90ed7bb92a6fd9061e1ebd3bb
Nguyen Luong Khang <96479575+Khangnekk@users.noreply.github.com>
2025-03-30 21:51:46 +0700

    home-client res

--- BPHN.MVC/app/src/components/BPHNHome.vue
+++ BPHN.MVC/app/src/components/BPHNHome.vue
@@ -1,53 +1,82 @@
-<script setup></script>
-
 <script setup>
-import { ref } from 'vue';
 
-const selectedArea = ref('');
+import { useI18n } from "vue-i18n";
+import { ref } from 'vue';
+import { Provinces } from "@/const";
+import router from "@/routers";
+import { useStore } from "vuex";
+const { t } = useI18n();
+const store = useStore();
+// const selectedArea = ref('');
+const isMobile = ref(store.getters["config/isMobile"]);
+const provinceId = ref(null);
+const lstProvinces = ref(Object.keys(Provinces).map(x => { return { id: parseInt(x), name: Provinces[parseInt(x)] } }));
+
+const goTo = (link) => {
+  router.push(link);
+}
 </script>
 <template>
   <div class="container">
-    <section class="banner">
-        <div class="w-100">
-            <img src="../assets/images/football-field.png" alt="Football Field" class="banner-img" />
-        </div>
-      <div class="d-flex flex-row w-100 justify-content-center">
-            <div class="booking-box">
-                <h1 class="fs-1 m-3 fw-bolder">Đặt sân nhanh chóng chỉ với vài bước đơn giản</h1>
-                <p class="fs-5 m-3">Hơn 100 sân bóng đang chờ đón những chiến binh sân cỏ</p>
-                <div class="d-flex justify-content-center">
-                    <el-select v-model="selectedArea" size="large" class="w-50 m-2">
-                        <option value="">Chọn khu vực đặt sân</option>
-                        <option value="hanoi">Hà Nội</option>
-                        <option value="hcm">TP. Hồ Chí Minh</option>
-                    </el-select>
-                    <el-button type="primary" size="large" class="m-2">Đặt sân ngay</el-button>
-                </div>
+    <section :class="isMobile ? '' : 'p-4'" class="banner">
+      <div class="w-100">
+        <img src="../assets/images/football-field.png" alt="Football Field" class="banner-img" :height="500" />
+      </div>
+      <div :class="isMobile ? 'booking-box mobile' : 'booking-box'">
+        <h1 class="fs-3 fw-bolder text-center m-0">Đặt sân nhanh chóng
+          với vài bước đơn giản</h1>
+        <p class="text-center">Hơn 100 sân bóng đang chờ đón những
+          chiến binh sân cỏ</p>
+        <div class="row" :style="isMobile?'':'padding: 0 50px'">
+          <div class="col-12 col-sm-12 col-md-12 col-lg-9">
+            <div class="mx-2">
+              <el-select v-model="provinceId" :placeholder="t('Province')" :no-data-text="t('NoData')" size="large"
+                class="w-100 mb-2">
+                <el-option v-for="item in lstProvinces" :key="item.id" :label="item.name" :value="item.id" />
+              </el-select>
             </div>
+          </div>
+          <div class="col-12 col-sm-12 col-md-12 col-lg-3">
+            <div class="mx-2">
+              <el-button type="primary" size="large" @click="goTo('/booking')" class="w-100 mb-2">Đặt sân ngay
+              </el-button>
+            </div>
+
+          </div>
         </div>
+
+      </div>
     </section>
 
     <section class="features">
-      <h2>Bạn là chủ sân bóng?</h2>
-      <p>Tham gia ngay BPHN để tối đa hóa lợi nhuận, tiết kiệm thời gian quản lý!</p>
-      <div class="features-list">
-        <div class="feature-item">
-          <img src="../assets/images/football-field.png" alt="Calendar" />
-          <h3>Cho phép mọi người đặt sân</h3>
-          <p>Khách hàng có thể đặt sân nhanh chóng, không lo trống giờ.</p>
+      <h2 class="text-center bg-info p-2 text-white">Bạn là chủ sân bóng?</h2>
+      <p class="m-0 fs-5 p-3">Tham gia ngay BPHN để tối đa hóa lợi nhuận, tiết kiệm thời gian quản lý!</p>
+      <div class="features-list row">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-4">
+          <div class="feature-item" :class="isMobile?'mx-2 mb-5':'mx-5'">
+            <img src="../assets/images/table-icon.png" alt="Calendar"/>
+            <h3 class="m-0 fs-4 text-center mb-3">Cho phép mọi người đặt sân</h3>
+            <p class="m-0 fs-5 ">Khách hàng có thể đặt sân nhanh chóng, không lo trống giờ.</p>
+          </div>
         </div>
-        <div class="feature-item">
-          <img src="../assets/images/football-field.png" alt="Management" />
-          <h3>Quản lý sân hiệu quả hơn</h3>
-          <p>Dễ dàng theo dõi lịch sân, doanh thu, khách hàng.</p>
+        <div class="col-12 col-sm-12 col-md-12 col-lg-4">
+          <div class="feature-item" :class="isMobile?'mx-2 mb-5':'mx-5'">
+            <img src="../assets/images/pitch-icon.png" alt="Management" />
+            <h3 class="m-0 fs-4 text-center mb-3">Quản lý sân hiệu quả hơn</h3>
+            <p class="m-0 fs-5">Dễ dàng theo dõi lịch sân, doanh thu, khách hàng.</p>
+          </div>
         </div>
-        <div class="feature-item">
-          <img src="../assets/images/football-field.png" alt="Order" />
-          <h3>Tích hợp order thêm đồ uống</h3>
-          <p>Tăng thêm lợi nhuận mà không cần tốn công quản lý.</p>
+        <div class="col-12 col-sm-12 col-md-12 col-lg-4">
+          <div class="feature-item" :class="isMobile?'mx-2 mb-5':'mx-5'">
+            <img src="../assets/images/invoice-icon.png" alt="Order" />
+            <h3 class="m-0 fs-4 text-center mb-3">Tích hợp order thêm đồ uống</h3>
+            <p class="m-0 fs-5">Tăng thêm lợi nhuận mà không cần tốn công quản lý.</p>
+          </div>
         </div>
       </div>
-      <button class="register-owner">Đăng ký tài khoản chủ sân ngay</button>
+      <div class="d-flex flex-row justify-content-center">
+        <el-button type="primary" size="large" @click="goTo('/')">Đăng ký tài khoản chủ sân ngay</el-button>
+      </div>
     </section>
 
     <section class="testimonials">
@@ -77,43 +106,52 @@ const selectedArea = ref('');
 </template>
 
 <style scoped>
-
 .banner {
-  text-align: center;
-  padding: 50px;
+  position: relative;
 }
+
 .banner-img {
   width: 100%;
-  height: 60vh;
   border-radius: 20px;
   object-fit: cover;
 }
+
 .booking-box {
-    width: 75%;
-    background: #ffffff;
-    padding: 20px;
-    border-radius: 30px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    position: relative;
-    top: -150px;
+  width: 75%;
+  background: #ffffff;
+  padding: 60px 60px 20px 60px;
+  border-radius: 30px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  position: absolute;
+  bottom: -150px;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
+
+.booking-box.mobile {
+  width: 90%;
+}
+
 .features {
-  text-align: center;
-  padding: 50px;
-  background: #f9f9f9;
+  margin-top: 125px;
 }
-.features-list {
+
+.feature-item {
+  background-color: #f2f2f2;
+  border-radius: 25px;
   display: flex;
-  justify-content: space-around;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 30px;
 }
-.testimonials {
-  padding: 50px;
-  text-align: center;
+
+.feature-item img {
+  width: 75%;
+  margin-top: -30px;
 }
-.footer {
-  background: #222;
-  color: white;
-  text-align: center;
-  padding: 20px;
+
+.features-list{
+  /* padding: 50px; */
 }
 </style>
